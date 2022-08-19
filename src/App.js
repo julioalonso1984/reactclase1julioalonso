918e0ac35b805e7d950ba8121fa561142fae28c2
julioalonso1984 <julsalonso@hotmail.com>
2022-08-19 20:42:16 -0300

    master

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+//import logo from './logo.svg';
+import './estilos.css';
 
 function App() {
+  let imagen = "heladera.jpg";
+  let titulo = "heladera";
+  let precio = "$90.000";
+  let descripcion = "no frost, inverter";
+  const producto = {id:1, image:imagen, title:titulo, price:precio, description:descripcion}
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1>Liquidación oferta</h1>
+      <p>Ultimos productos disponibles</p>
+      <div className='row'>
+        <div className='col-md-4 offset-md-2'>
+          <img src={producto.image} alt={producto.title} className="img-fluid" />
+        </div>
+        <div className="col-md-4">
+          <h1>{producto.title}</h1>
+          <p>{producto.price}</p>
+          <p>{producto.description}</p>
+        </div>        
+      </div>
+      
     </div>
   );
 }
